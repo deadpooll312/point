e302b3d66159e65e56e82f7e8ba51c9b67f3c85c
daryagolubewa <darya.golubewa@gmail.com>
2020-05-25 14:08:21 +0300

    DITAODS19-1851 корректирует дизайн пагинации

--- src/components/pagination.js
+++ src/components/pagination.js
@@ -3,5 +3,5 @@ import {Pagination} from "antd";
 
 // eslint-disable-next-line react/prop-types
 export const PaginationComponent = ({onChange}) => (
-  <Pagination onChange={onChange} defaultCurrent={1} total={50} />
+  <Pagination size="small" onChange={onChange} defaultCurrent={1} total={50} />
 );
