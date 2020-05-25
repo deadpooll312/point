3411d2dd5fe05bc3190264d6d3f50671b22068d4
daryagolubewa <darya.golubewa@gmail.com>
2020-05-25 13:16:45 +0300

    DITAODS19-1875 скрывает значок сохранения

--- src/pages/parks/components/parks.header.js
+++ src/pages/parks/components/parks.header.js
@@ -1,12 +1,12 @@
 import React, {useCallback} from "react";
 import {Typography} from "antd";
-import {MoreOutlined, SaveOutlined} from "@ant-design/icons";
+import {MoreOutlined} from "@ant-design/icons";
 import {inject, observer} from "mobx-react";
 const {Title} = Typography;
 
 export const ParksHeader = inject("store")(
   observer(({store: {sidebar}}) => {
-    const save = useCallback(() => {}, []);
+    // const save = useCallback(() => {}, []);
 
     const callMenu = useCallback(() => {
       sidebar.toggleDrawer(true);
@@ -16,7 +16,7 @@ export const ParksHeader = inject("store")(
       <div className="parks__title">
         <Title level={3}>Парки видовые</Title>
         <div>
-          <SaveOutlined onClick={save} className="icon" />
+          {/* <SaveOutlined onClick={save} className="icon" /> */}
           <MoreOutlined onClick={callMenu} className="icon" />
         </div>
       </div>
