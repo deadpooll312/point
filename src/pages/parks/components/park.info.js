c74053a2ad53bb8fc5982a5e1291f61335a0c618
Вячеслав Д <sl.d@mail.ru>
2020-05-28 19:42:53 +0600

    сделал чтобы цвета у селектора состояния территории менялись в зависимости от значения

--- src/pages/parks/components/park.info.js
+++ src/pages/parks/components/park.info.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import {Col, Row} from "antd";
 import {ProgressComponent} from "../../../components/progress";
 import {SelectComponent} from "../../../components/select";
@@ -9,11 +9,16 @@ import {inject, observer} from "mobx-react";
 export const ParkInfo = inject("store")(
   observer(({store: {parks}}) => {
     const [data, setData] = useState({});
-
+    
     useEffect(() => {
       setData(parks.singlePark);
     }, [parks.singlePark]);
-
+    
+    
+    const tempItem = crowdColorNames.find((value) => value.value === data.crowdColor);
+    const colorClass = tempItem ? tempItem.className : "";
+    
+    
     return (
       <div className="park-info">
         <Row>
@@ -40,15 +45,17 @@ export const ParkInfo = inject("store")(
             </div>
             <div className="park-info__outline">
               <span>Состояние (цвет) территории</span>
-              {data.crowdColor && (
-                <SelectComponent
-                  data={crowdColorNames}
-                  labelInValue={true}
-                  defaultValue={{value: data.crowdColor}}
-                  handleChange={({value}) => parks.updateCrowdColorName(value)}
-                  placeholder={data.crowdColorName}
-                />
-              )}
+              <div className={colorClass}>
+                {data.crowdColor && (
+                  <SelectComponent
+                    data={crowdColorNames}
+                    labelInValue={true}
+                    defaultValue={{value: data.crowdColor}}
+                    handleChange={({value}) => parks.updateCrowdColorName(value)}
+                    placeholder={data.crowdColorName}
+                  />
+                )}
+              </div>
             </div>
             <div className="park-info__outline">
               <span>MAX посетителей</span>
@@ -56,7 +63,7 @@ export const ParkInfo = inject("store")(
             </div>
           </Col>
         </Row>
-        <br />
+        <br/>
         <h2>Ответственные лица</h2>
         <Row>
           <Col span={12}>
@@ -74,7 +81,7 @@ export const ParkInfo = inject("store")(
             </div>
           </Col>
         </Row>
-        <br />
+        <br/>
         <h2>Оценка загруженности территории</h2>
         <Row gutter={16}>
           <Col span={12}>
@@ -123,5 +130,5 @@ export const ParkInfo = inject("store")(
         </Row>
       </div>
     );
-  })
+  }),
 );
