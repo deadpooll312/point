4d14c181f54341f0d922ae2a732c86d7cff75f5d
kapkozheckshenov <deadpollkgz@gmail.com>
2020-06-08 20:54:16 +0600

    map layout fixed

--- src/consts/map.const.js
+++ src/consts/map.const.js
@@ -5,6 +5,7 @@ const lat = 55.756288;
 const source = "EPSG:4326";
 const destination = "EPSG:3857";
 
+export const urlTemplate = `tile1.maps.2gis.com/tiles?layerType=nc&x={x}&y={y}&z={z}`;
 export const parkMap = "park-map";
 export const minZoom = 9;
 export const zoom = 10;
--- src/services/map.service.js
+++ src/services/map.service.js
@@ -1,22 +1,55 @@
 import {Tile as TileLayer} from "ol/layer";
-import {OSM} from "ol/source";
 import Map from "ol/Map";
 import View from "ol/View";
 import Zoom from "ol/control/Zoom";
+import XYZSource from "ol/source/XYZ";
 // local files
-import {center, minZoom, parkMap, zoom} from "../consts/map.const";
+import {center, minZoom, parkMap, urlTemplate, zoom} from "../consts/map.const";
+export const tileId = "tiles";
 
-export const layers = [
-  new TileLayer({
-    source: new OSM(),
-  }),
-];
+function getWGSTiles(urlTemplate) {
+  return createTiles2GIS(urlTemplate);
+}
 
-export const createMap = () =>
-  new Map({
-    layers: layers,
-    target: parkMap,
-    logo: false,
-    view: new View({center, minZoom, zoom}),
-    controls: [new Zoom()],
+function switchMapSRS(map, urlTemplate) {
+  setTileLayer(map, getWGSTiles(urlTemplate));
+  map.updateSize();
+}
+
+export function getLayerById(map, id) {
+  return map
+    .getLayers()
+    .getArray()
+    .find((l) => l.get("id") === id);
+}
+
+export function createTiles2GIS(urlTemplate) {
+  return new TileLayer({
+    id: tileId,
+    source: new XYZSource({
+      url: `${location.protocol}//${urlTemplate}`,
+      crossOrigin: "anonymous",
+    }),
+    opacity: 0.7,
   });
+}
+
+export function setTileLayer(map, layer) {
+  const tiles = getLayerById(map, tileId);
+  map.removeLayer(tiles);
+  map.getLayers().insertAt(0, layer);
+  return map;
+}
+
+export const createMap = () => {
+  const mapNew = new Map({
+    logo: false,
+    controls: [new Zoom()],
+    target: parkMap,
+    view: new View({center, minZoom, zoom}),
+  });
+
+  switchMapSRS(mapNew, urlTemplate);
+
+  return mapNew.updateSize();
+};
