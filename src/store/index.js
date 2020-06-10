aa9bb34733eab37dd41bb959156209a535ae0247
kapkozheckshenov <deadpollkgz@gmail.com>
2020-06-10 20:48:27 +0600

    DITAODS19-1998 fixed

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,14 @@
 import SidebarStore from "./sidebar/sidebar.store";
 import ParksStore from "./parks/parks.store";
 import AuthStore from "./auth/auth.store";
+import MapStore from "./map/map.store";
 
 class RootStore {
   constructor() {
     this.sidebar = SidebarStore;
     this.parks = ParksStore;
     this.auth = AuthStore;
+    this.map = MapStore;
   }
 }
 
