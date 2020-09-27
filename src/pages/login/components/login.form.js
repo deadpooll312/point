d3dfc40b0f3f5123573487776f7cc02a28d16aee
deadpoollkgz312 <deadpollkgz@gmail.com>
2020-09-27 21:45:11 +0600

    feat: created button component

--- src/pages/login/components/login.form.js
+++ src/pages/login/components/login.form.js
@@ -1,11 +1,13 @@
 import React from "react";
 import {inject, observer} from "mobx-react";
+import Button from "../../../components/button";
 
 export const LoginForm = inject("store")(
   observer(({store: {auth}}) => {
     return (
       <div className="login-form-container">
         <h1>Login</h1>
+        <Button name="Назначить" />
       </div>
     );
   })
